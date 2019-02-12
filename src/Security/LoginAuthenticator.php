31d9e48851787e7073ef4ef76726b849bf2416a8
jpelage2018 <pelage35@gmail.com>
2019-02-12 16:27:43 +0100

    -- ajout css -- accueil -- modif security

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -85,7 +85,7 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('app-login'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
 
     }
 
