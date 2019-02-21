9d339111bb4032357d2b9132948878ef6f73c705
dpezo2018 <dylanpezo972@hotmail.com>
2019-02-21 16:33:59 +0100

    -- gestion des groupes

--- src/Controller/GroupeController.php
+++ src/Controller/GroupeController.php
@@ -69,9 +69,100 @@ class GroupeController extends AbstractController
         $groupRepo = $this->getDoctrine()->getRepository(Groupe::class);
         $groupe = $groupRepo->find($id);
 
-        $groupe->addParticipant($this->getUser());
-        $this->addFlash('success', 'Vous a bien été inscrit au groupe : '. $groupe->getNom());
+
+        if(!in_array($this->getUser(),$groupe->getParticipants()->toArray())) {
+
+
+            $groupe->addParticipant($this->getUser());
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($groupe);
+            $em->flush();
+            $this->addFlash('success', 'Vous a bien été inscrit au groupe : ' . $groupe->getNom());
+
+            return $this->redirectToRoute('home');
+
+        }
 
         return $this->redirectToRoute('home');
+
     }
+
+
+    /**
+     * @Route("/groupeDesister/{id}",
+     *     name="desinscrire",
+     *     requirements={"id": "\d+"},
+     *     methods={"GET"})
+     */
+    public function desinscrire(int $id){
+
+        $groupRepo = $this->getDoctrine()->getRepository(Groupe::class);
+        $groupe = $groupRepo->find($id);
+
+
+        if(in_array($this->getUser(),$groupe->getParticipants()->toArray())) {
+
+
+            $groupe->removeParticipant($this->getUser());
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            $this->addFlash('success', 'Vous avez bien été désinscrit au groupe : ' . $groupe->getNom());
+
+            return $this->redirectToRoute('home');
+
+        }
+
+        return $this->redirectToRoute('home');
+
+    }
+
+
+
+
+
+
+    /**
+     * @Route("/mesGroupes", name="mesGroupes")
+     */
+    public function mesGroupes(){
+
+
+
+        $groupes = $this->getUser()->getGroupes();
+
+        return $this->render('user/mesGroupes.html.twig', [
+            'groupes' => $groupes,
+            'user' => $this->getUser()
+        ]);
+
+    }
+
+    /**
+     * @Route("/deleteGroupe/{id}", name="supprGroupe",
+     *     requirements={"id": "\d+"},
+     *     methods={"GET"}))
+     */
+    public function deleteGroupe(int $id){
+
+
+        $groupRepo = $this->getDoctrine()->getRepository(Groupe::class);
+        $groupe = $groupRepo->find($id);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($groupe);
+        $em->flush();
+
+
+        $this->addFlash('success', 'Vous avez bien supprimer votre groupe : ' . $groupe->getNom());
+
+        return $this->redirectToRoute('home');
+
+
+    }
+
+
 }
